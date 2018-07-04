883e84eb44b002f5494646b80eeedb7f04dadab1
admin <admin@admin-HP>
2018-07-04 18:31:22 +0900

    usersDao更新

--- src/main/java/jp/co/axiz/web/dao/UsersDao.java
+++ src/main/java/jp/co/axiz/web/dao/UsersDao.java
@@ -2,6 +2,9 @@ package jp.co.axiz.web.dao;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import jp.co.axiz.web.entity.Users;
@@ -9,12 +12,35 @@ import jp.co.axiz.web.entity.Users;
 @Repository
 public class UsersDao {
 
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	private final String SQL_SELECT_ID = "SELECT * FROM users WHERE user_id = ?";
+
+	private final String SQL_SELECT_ID_PASS = "SELECT * FROM users "
+						+ "WHERE user_id = ? AND password = ? AND delete_flg = 0";
+
+
+
 	public List<Users> find(Users users){
 		return null;
 	}
 
 	public Users findById(String userId) {
-		return null;
+		List<Users> list = jdbcTemplate.query(SQL_SELECT_ID,
+				new BeanPropertyRowMapper<Users>(Users.class),
+				userId);
+
+		return list.get(0);
+	}
+
+	public Users findByIdPass(Users users) {
+		List<Users> list = jdbcTemplate.query(SQL_SELECT_ID_PASS,
+				new BeanPropertyRowMapper<Users>(Users.class),
+				users.getUserId(),
+				users.getPassword());
+
+		return list.get(0);
 	}
 
 	public void register(Users users) {
--- src/main/java/jp/co/axiz/web/entity/Images.java
+++ src/main/java/jp/co/axiz/web/entity/Images.java
@@ -5,4 +5,24 @@ public class Images {
 	private Integer imageId;
 	private Integer articleId;
 	private String imagePath;
+	public Integer getImageId() {
+		return imageId;
+	}
+	public void setImageId(Integer imageId) {
+		this.imageId = imageId;
+	}
+	public Integer getArticleId() {
+		return articleId;
+	}
+	public void setArticleId(Integer articleId) {
+		this.articleId = articleId;
+	}
+	public String getImagePath() {
+		return imagePath;
+	}
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+
+
 }
