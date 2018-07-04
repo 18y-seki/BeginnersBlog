97f886fa4f88ce18c5d13ed1d0b407b8b59eb82c
admin <admin@admin-HP>
2018-07-04 19:24:50 +0900

    ArticleDao更新

--- src/main/java/jp/co/axiz/web/dao/ArticleDao.java
+++ src/main/java/jp/co/axiz/web/dao/ArticleDao.java
@@ -2,6 +2,9 @@ package jp.co.axiz.web.dao;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import jp.co.axiz.web.entity.Article;
@@ -9,9 +12,21 @@ import jp.co.axiz.web.entity.Article;
 @Repository
 public class ArticleDao {
 
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	private final String SQL_SELECT_ALL = "SELECT * FROM article";
+
 
 	public List<Article> find(Article article){
 		return null;
+	}
+
+	public List<Article> findAll(){
+		List<Article> list = jdbcTemplate.query(SQL_SELECT_ALL,
+				new BeanPropertyRowMapper<Article>(Article.class));
+
+		return list;
 	}
 
 	public Article findById(Integer id) {
--- src/main/java/jp/co/axiz/web/dao/CommentDao.java
+++ src/main/java/jp/co/axiz/web/dao/CommentDao.java
@@ -2,6 +2,9 @@ package jp.co.axiz.web.dao;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import jp.co.axiz.web.entity.Comments;
@@ -9,12 +12,26 @@ import jp.co.axiz.web.entity.Comments;
 @Repository
 public class CommentDao {
 
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	private final String SQL_SELECT_ARTICLEID = "SELECT * FROM comment WHERE article_id = ?";
+
+
 	public List<Comments> find(Comments comments){
 		return null;
 	}
 
 	public Comments findById(Integer id) {
 		return null;
+	}
+
+	public List<Comments> findByArticleId(Integer articleId) {
+		List<Comments> list = jdbcTemplate.query(SQL_SELECT_ARTICLEID,
+				new BeanPropertyRowMapper<Comments>(Comments.class),
+				articleId);
+
+		return list;
 	}
 
 	public void register(Comments comments) {
